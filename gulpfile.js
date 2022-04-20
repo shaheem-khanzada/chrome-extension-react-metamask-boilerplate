ce6945899e33f2a19a9892557ca6b96a4eaa5a83
Shaheem Khan <mshaheemkz@gmail.com>
2022-04-21 04:51:18 +0500

    added live server

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,60 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
 const babelify = require('babelify');
+const open = require('open');
 const source = require('vinyl-source-stream');
-const eslint = require('gulp-eslint');
+const gulpESLintNew = require('gulp-eslint-new');
+const connect = require('gulp-connect');
 
-const env = "./dist";
+var config = {
+  name: 'Nft Prize Locker',
+  root: 'dist',
+  port: 8080,
+  devBaseUrl: 'http://localhost',
+}
 
-gulp.task('lint', (done) => {
-  gulp.src('./src/**/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-
-  done();
+gulp.task('lint', function () {
+  return gulp.src('./src/**/*.js')
+    .pipe(gulpESLintNew())
+    .pipe(gulpESLintNew.format())
+    .pipe(gulpESLintNew.failAfterError());
 });
 
-gulp.task('build', gulp.series('lint'), function (done) {
+gulp.task('build', function (done) {
   browserify({ entries: './src/index.js', debug: true }).plugin('css-modulesify', {
-    o: env + '/nftprizelocker.css'
+    o: config.root + '/nftprizelocker.css'
   }).transform(babelify)
     .bundle()
     .pipe(source('nftprizelocker.bundle.js'))
-    .pipe(gulp.dest(env + '/js'));
+    .pipe(gulp.dest(config.root + '/js'))
+    .pipe(connect.reload());
 
   done();
 });
 
 gulp.task('copy-manifest', function (done) {
   gulp.src('./manifest.json')
-    .pipe(gulp.dest(env));
+    .pipe(gulp.dest(config.root));
 
   done()
 });
 
+gulp.task('connect', function (done) {
+  connect.server({
+    name: 'Nft Prize Locker',
+    root: 'dist',
+    port: config.port,
+    devBaseUrl: config.devBaseUrl,
+    livereload: true
+  });
+
+  open(`${config.devBaseUrl}:${config.port}`)
+
+  done();
+});
+
 gulp.task('watch', function () {
-  gulp.watch('./src/**/*.js', gulp.series('build'));
+  gulp.watch('./src/**/*.js', gulp.series('lint', 'build'));
   gulp.watch('./src/**/*.css', gulp.series('build'));
 });
 
